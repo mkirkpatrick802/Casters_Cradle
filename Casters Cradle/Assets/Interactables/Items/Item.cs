58357d7c2afa94180dfa18080ed4aecbf1468ee7
Michael <mkirkpatrick802@gmail.com>
2022-05-08 23:36:41 -0400

    Progress

---
+++ Casters Cradle/Assets/Interactables/Items/Item.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Items/General Item", order = 0)]
+public class Item : ScriptableObject
+{
+    [Header("General Settings")]
+    public new string name;
+    public Sprite icon;
+    public ItemType itemType;
+    public bool isStackable;
+    public int amount;
+}
+
+public enum ItemType
+{
+    Null,
+    Gold,
+    SpellComponent,
+    Spell
+}
--- Casters Cradle/Assets/Managers/Systems/ReferenceManager.cs
+++ Casters Cradle/Assets/Managers/Systems/ReferenceManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ReferenceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ReferenceManager instance { private set; get; }
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Transform dropItem;
 }
